a4675d4086b1a5fa2638469563325cc1c9d9b131
Mh Asif Kamal <56258975+mhasif0786@users.noreply.github.com>
2022-04-07 08:31:14 +0530

    Create fine.go
    new_code

---
+++ fine.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main()  {
+	fmt.Println("hello ,How are you?")
+	var username string = "Mh Asif Kamal"
+	var college string = "Manit,Bhopal"
+	fmt.Println("My name is ",username)
+	fmt.Println("I have complete M.Tech from ",college)
+	fmt.Printf("hey the type of college variable is: %T \n", college)
+	fmt.Printf("hey the type of username variable is: %T \n", username)
+}
